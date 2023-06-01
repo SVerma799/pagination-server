ad0832d51c1627a6fb979cd763ec6d927adf1d17
Shubham Verma <49226983+SVerma799@users.noreply.github.com>
2023-06-01 12:52:59 -0700

    Changes done to server side to do pagination

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,8 +1,68 @@
+import { NextRequest } from "next/server";
 import { User } from "../../../../types/user";
 import clientPromise from "../../../../utils/connectDB";
+import { useRouter } from "next/router";
+import { Sort, SortDirection } from "mongodb";
 
-export const GET = async (req: any) => {
-  return new Response(JSON.stringify({ message: "Hello from users" }), {
+export const GET = async (req: NextRequest) => {
+  const { searchParams } = new URL(req.url);
+  const pageString = searchParams.get("page");
+  const page: number = pageString ? parseInt(pageString, 10) : 1;
+
+  const limitString = searchParams.get("limit");
+  const limit: number = limitString ? parseInt(limitString, 10) : 10;
+
+  const sortString = searchParams.get("sort");
+
+  let sort: string;
+  if (sortString != null) {
+    sort = searchParams.get("sort")?.valueOf() as string;
+  } else {
+    sort = "name";
+  }
+
+  console.log("sort", sort);
+
+  const orderString = searchParams.get("order");
+  const order: SortDirection = orderString
+    ? (parseInt(orderString, 10) as SortDirection)
+    : (1 as SortDirection);
+
+  const client = await clientPromise;
+  if (!process.env.DB_NAME) {
+    return new Response(
+      JSON.stringify({
+        message: "Invalid/Missing environment variable: DB_NAME",
+      }),
+      {
+        headers: { "Content-Type": "application/json" },
+        status: 500,
+      }
+    );
+  }
+
+  const db = client.db(process.env.DB_NAME);
+
+  const skip = page * limit - limit;
+
+  console.log("sort", sort);
+
+  const sortField: Sort = sort; // Replace 'fieldName' with the actual field name you want to use for sorting
+  const orderField: SortDirection = order; // Replace 1 with the desired sort direction (1 for ascending, -1 for descending)
+
+  console.log("sortField", sortField);
+
+  console.log("orderField", orderField);
+
+  const users = await db
+    .collection<User>("users")
+    .find({})
+    .limit(limit)
+    .skip(skip)
+    .toArray();
+
+  //.sort({ [sortField]: orderField })
+  return new Response(JSON.stringify(users), {
     headers: { "Content-Type": "application/json" },
     status: 200,
   });
